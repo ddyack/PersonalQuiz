5c26f1f5b004e465e0cff0861a97f16673a65261
ddyack <ddyack@ozon.ru>
2020-09-01 15:28:03 +0300

    init

---
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 31.08.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    // 1. Избавиться от кнопкп возврата
+    // 2. Передать сюда массив с выбранными ответами
+    // 3. Определить то животное, которое встречается чаще всего
+    // 4. Отобразить результаты
+    // 5. Подумать над логикой определени индекса в соответсвии с диапазоном
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
